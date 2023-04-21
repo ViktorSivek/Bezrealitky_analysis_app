ce56fa3d8303c56decab5bae8546034e646e4267
Viktor Sívek <sivv01@vse.cz>
2023-04-21 13:11:56 +0200

    update3

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Wow to run:
 2. myenv\Scripts\activate
 3. pip install -r requirements.txt
 4. python app/main.py
+
+If you have scraped data already, dash app will run.
+If you have not scraped data yet, scraping will run and dash app after that.
--- app/main.py
+++ app/main.py
@@ -36,6 +36,7 @@ app = dash.Dash(__name__)
 # Define the layout
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
+
     html.Div([
         html.Label("X-axis:"),
         dcc.Dropdown(
@@ -54,7 +55,25 @@ app.layout = html.Div([
         ),
     ], style={'width': '48%', 'display': 'inline-block'}),
 
-    dcc.Graph(id='graph-content')
+    dcc.Graph(id='graph-content'),
+
+    # Add new visualizations here
+    html.H2("Number of Properties by Building Type", style={'textAlign': 'center'}),
+    dcc.Graph(id='bar-chart'),
+
+    html.H2("Correlation Heatmap", style={'textAlign': 'center'}),
+    dcc.Graph(id='heatmap'),
+
+    html.H2("Distribution of Property Layouts", style={'textAlign': 'center'}),
+    dcc.Graph(id='pie-chart'),
+
+    html.H2("Area Distribution (Box plot)", style={'textAlign': 'center'}),
+    dcc.Graph(id='box-plot'),
+
+    html.H2("Area Distribution (Histogram)", style={'textAlign': 'center'}),
+    dcc.Graph(id='histogram'),
+
+    html.Button("Update Visualizations", id="update-visualizations"),
 ])
 
 # Update the graph based on the selected column
@@ -65,6 +84,29 @@ app.layout = html.Div([
 )
 def update_graph(x_value, y_value):
     return px.scatter(df, x=x_value, y=y_value)
+
+# Create a new column with numeric area values
+df['PLOCHA_NUM'] = df['PLOCHA'].str.extract('(\d+)').astype(float)
+df['CENA_NUM'] = df['CENA'].str.replace(' ', '').str.extract('(\d+)').astype(float)
+
+# Create new visualizations
+fig_bar = px.bar(df.groupby('TYP BUDOVY').size().reset_index(name='count'), x='TYP BUDOVY', y='count')
+fig_heatmap = px.imshow(df[['PLOCHA_NUM', 'PODLAŽÍ', 'CENA_NUM']].astype(float).corr())
+fig_pie = px.pie(df, names='DISPOZICE')
+fig_box = px.box(df, y='PLOCHA_NUM')
+fig_histogram = px.histogram(df, x='PLOCHA_NUM', nbins=20)
+
+# Update the new visualizations
+@app.callback(
+    Output('bar-chart', 'figure'),
+    Output('heatmap', 'figure'),
+    Output('pie-chart', 'figure'),
+    Output('box-plot', 'figure'),
+    Output('histogram', 'figure'),
+    Input('update-visualizations', 'n_clicks')  # Add a new input for the update function
+)
+def update_new_visualizations(n_clicks=None):
+    return fig_bar, fig_heatmap, fig_pie, fig_box, fig_histogram
 
 # Run the Dash app
 if __name__ == '__main__':
